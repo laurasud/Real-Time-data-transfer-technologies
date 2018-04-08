b0ec2d08855eee713b68ec6b57f775645f235ea1
laurasud <laurasudeikyte@gmail.com>
2018-04-08 17:44:43 +0300

    Reconnection
    Reconnecting in case of error or connection close every 5sec

--- WebSockets/frontend.js
+++ WebSockets/frontend.js
@@ -1,33 +1,42 @@
 $(function () {
-	// if user is running mozilla then use it's built-in WebSocket
-	window.WebSocket = window.WebSocket || window.MozWebSocket;
-	
-	var connection = new WebSocket('ws://localhost:3000'); //sinchroninis ar asinchroninis
-	var data = {};
-	var url = document.URL;
-	var mousemove; //mousemove event
-	
+	start();
+	function start(websocketServerLocation){
+		
+		// if user is running mozilla then use it's built-in WebSocket
+		window.WebSocket = window.WebSocket || window.MozWebSocket;
 
-	connection.onopen = function () {
-		console.log("connection opened");
-		mousemove = window.addEventListener("mousemove", function(event){ //count
-		data = {
-			"url": url,
-			"time": Date(),
-			"mousepositionX": event.pageX,
-			"mousepositionY": event.pageY
+		var connection = new WebSocket('ws://localhost:3000'); //sinchroninis ar asinchroninis
+		
+		var data = {};
+		var url = document.URL;
+		var mousemove; //mousemove event
+		var conn;
+
+		connection.onopen = function () {
+			conn=true;
+			console.log("connection opened");
+			mousemove = window.addEventListener("mousemove", function(event){ //count
+			data = {
+				"url": url,
+				"time": Date(),
+				"mousepositionX": event.pageX,
+				"mousepositionY": event.pageY
+			}
+			if (conn == true){
+			connection.send(JSON.stringify(data));	//pasidaryti masyva	
+			}
+		});
+		 };
+
+		connection.onerror = function (error) {
+			document.removeEventListener("mousemove", mousemove);
+			setTimeout(function(){start(websocketServerLocation)}, 5000); //reconnect every 5sec
+		};
+		
+		connection.onclose = function () {
+			document.removeEventListener("mousemove", mousemove);
+			conn = false;
+			setTimeout(function(){start(websocketServerLocation)}, 5000); //reconnect every 5sec
 		}
-		connection.send(JSON.stringify(data));	//pasidaryti masyva	
-	});
-	 };
-
-	connection.onerror = function (error) {
-		console.log("connection error");
-		document.removeEventListener("mousemove", mousemove);
-	};
-	
-	connection.onclose = function () {
-		console.log("connection closed");
-		document.removeEventListener("mousemove", mousemove);
 	}
 });
